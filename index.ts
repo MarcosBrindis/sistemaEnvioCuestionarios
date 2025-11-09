2e098bb603250decc03b01f90305fe93e1dcbd3b
FredyDelarosa <fredydelarosarojascetis138@gmail.com>
2025-11-08 23:31:41 -0600

    feat: conexion del proyecto al servidor de la base de datos

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
-//este es el index
+import express from 'express';
+import { testConnection } from './src/core/db/mysl/connection';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+async function startServer() {
+    await testConnection();
+    
+    app.get('/', (_req, res) => {
+        res.send('servidor iniciado');
+    });
+
+    app.listen(port, () => {
+        console.log('servidor corriendo en http://localhost:' + port);
+    })
+}
+
+startServer();
