89d3351ee5e465bc830aac77d66d245d251d6b66
FredyDelarosa <233317@ids.upchiapas.edu.mx>
2026-02-09 21:04:08 -0600

    fix: auth

--- src/auth/domain/port/authRepository.ts
+++ src/auth/domain/port/authRepository.ts
@@ -4,5 +4,5 @@ export interface AuthRepository {
   /**
    * @returns El egresado si las credenciales son correctas, null si no.
    */
-  validateCredentials(matricula: string, curp: string): Promise<Egresado | null>;
+  validateCredentials(curp: string): Promise<Egresado | null>;
 }
--- src/auth/infrastructure/database/mysql/AuthRepositoryMySQL.ts
+++ src/auth/infrastructure/database/mysql/AuthRepositoryMySQL.ts
@@ -3,10 +3,10 @@ import { Egresado } from '../../../../egresado/domain/model/egresado';
 import { MysqlConnection } from '../../../../core/db/mysl/connection';
 
 export class AuthRepositoryMySQL implements AuthRepository {
-  async validateCredentials(matricula: string, curp: string): Promise<Egresado | null> {
+  async validateCredentials(curp: string): Promise<Egresado | null> {
     const [rows]: any = await MysqlConnection.execute(
-      `SELECT * FROM egresado WHERE matricula = ? AND curp = ? AND is_active = TRUE LIMIT 1`,
-      [matricula, curp]
+      `SELECT * FROM egresado WHERE curp = ? AND is_active = TRUE LIMIT 1`,
+      [curp]
     );
     return rows.length > 0 ? rows[0] : null;
   }
